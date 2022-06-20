75db62ab98b40695918bc43c7e2f35182c788214
hyebinjo <hyebinjo91@gmail.com>
2022-06-20 15:25:41 +0900

    Assignment2 - GNB

---
+++ src/components/GNB.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MdHome } from 'react-icons/md';
+import { IoPaperPlaneOutline } from 'react-icons/io5';
+import { CgAddR } from 'react-icons/cg';
+import { ImCompass2 } from 'react-icons/im';
+import { FaRegHeart } from 'react-icons/fa';
+
+const GNB = () => {
+  return (
+    <Nav>
+      <Container>
+        <Img
+          src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+          alt="instargram logo"
+        />
+        <Input type="text" placeholder="검색" />
+        <Icons>
+          <MdHome />
+          <IoPaperPlaneOutline />
+          <CgAddR />
+          <ImCompass2 />
+          <FaRegHeart />
+          <Button>Logout</Button>
+        </Icons>
+      </Container>
+    </Nav>
+  );
+};
+
+export default GNB;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #ffffff;
+  border-bottom: 1px solid rgb(219, 219, 219);
+`;
+
+const Container = styled.div`
+  width: 975px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  @media screen and (max-width: 975px) {
+    width: 100%;
+  }
+  @media screen and (max-width: 415px) {
+    padding: 0;
+  }
+`;
+
+const Img = styled.img`
+  width: 102px;
+  margin-right: 24px;
+`;
+
+const Input = styled.input`
+  background-color: #efefef;
+  width: 268px;
+  height: 36px;
+  padding: 0 16px;
+  border-radius: 8px;
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-width: 278px;
+  padding-left: 24px;
+  font-size: 24px;
+  @media screen and (max-width: 640px) {
+    padding-left: 0;
+  }
+  @media screen and (max-width: 414px) {
+    min-width: 0;
+    width: 278px;
+  }
+`;
+
+const Button = styled.button`
+  border-radius: 4px;
+`;
--- src/pages/Mains.jsx
+++ src/pages/Mains.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import GNB from '../components/GNB';
 
 const Main = () => {
-  return <h1>메인페이지</h1>;
+  return (
+    <MainPage>
+      <GNB />
+      <h1>메인페이지</h1>
+    </MainPage>
+  );
 };
 
 export default Main;
+
+const MainPage = styled.main`
+  background-color: #fafafa;
+`;
