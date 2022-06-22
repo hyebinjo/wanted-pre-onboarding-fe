f49c0e9d631acc914a0bce2c95e910f781eef02d
hyebinjo <hyebinjo91@gmail.com>
2022-06-22 15:48:07 +0900

    Main페이지에서 userAccount 없을시 Login페이지로 navigate 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getStorage } from '../utils/storage';
 import styled from 'styled-components';
 import GNB from '../components/GNB';
 import Feeds from '../components/Feeds';
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    !getStorage('userAccount') && navigate('../');
+  }, []);
+
   return (
     <MainPage>
       <GNB />
